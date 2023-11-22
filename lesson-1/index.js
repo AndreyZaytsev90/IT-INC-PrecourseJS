697e8db77e66692a6fd59e6792abd09e3b5955cd
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2023-11-22 21:25:17 +0300

    update file

---
+++ lesson-1/index.js
@@ -0,0 +1,8 @@
+  //data
+  let name = "Andrey"
+  let graduatesCount = 2000
+  let areYouChampion = true
+
+  //UI, render
+  document.write('<h1>', name, "</h1>")
+  document.write('<input type="number" value="' + graduatesCount + '">')
