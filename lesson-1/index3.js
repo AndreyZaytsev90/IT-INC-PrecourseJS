7d23d66775b15992186304a6b9432ce73c10b648
zaytsev <zaicev@mniti.ru>
2023-11-23 11:13:37 +0300

    Update file

---
+++ lesson-1/index3.js
@@ -0,0 +1,16 @@
+//Objects
+
+let student = {
+    info: {
+        firstName: 'Andrey',
+        lastName: 'Зайцев'
+    },
+    address: {
+        country: 'Russia',
+        city: 'Moscow'
+    }
+}
+
+document.write('Имя студента: ', student.info.firstName)
+document.write('Фамилия студента: ', student.info.lastName)
+document.write('Адрес студента: ', student.address.city)
