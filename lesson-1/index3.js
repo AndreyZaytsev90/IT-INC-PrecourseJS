a3c7f491866066c46b69fd4ba84721a862a477f8
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2023-12-11 20:54:10 +0300

    Update project

--- lesson-1/index3.js
+++ lesson-1/index3.js
@@ -1,5 +1,5 @@
 //Objects
-
+//data
 let student = {
     info: {
         firstName: 'Andrey',
@@ -13,6 +13,9 @@ let student = {
 student.info.firstName = 'Olga'
 student.info.firstName = 'Andrey'
 
+//render
 document.write('<b>Имя студента:</b> ', student.info.firstName, '<br>')
 document.write('<b>Фамилия студента:</b> ', student.info.lastName, '<br>')
 document.write('<b>Адрес студента:</b> ', student.address.city, '<br>')
+
+console.log(student.info.firstName)
