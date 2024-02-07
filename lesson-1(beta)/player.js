0ba3b7299e710cc4ac084936f92fcfc1cce5a336
zaytsev <zaicev@mniti.ru>
2024-02-07 14:47:44 +0300

    Update file

--- lesson-1(beta)/player.js
+++ lesson-1(beta)/player.js
@@ -9,13 +9,13 @@ let playlist = {
         {
             artist: "Eminem",
             title: "Rap God",
-            fileUrl: ".mp3",
+            fileUrl: "Eminem_-_Rap_God_.mp3",
             isHot: false
         },
         {
             artist: "50 cent",
             title: "In da Club",
-            fileUrl: ".mp3",
+            fileUrl: "50_Cent_-_In_Da_Club.mp3",
             isHot: true
         },
     ]
