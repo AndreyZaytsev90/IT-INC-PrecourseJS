a444d8568dde4c30b1155228fbc16373ae34c710
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2024-02-11 20:10:41 +0300

    update file

--- lesson-1(beta)/player.js
+++ lesson-1(beta)/player.js
@@ -1,3 +1,4 @@
+//data
 let playlist = {
     title: "Hip-hop Hits",
     coverImageUrl: "./hip_hop_hits.jpg",
@@ -22,34 +23,72 @@ let playlist = {
         },
     ]
 }
-let playListTitleElement = document.createElement('h2')
-playListTitleElement.append(playlist.title)
-document.body.append(playListTitleElement)
+let playlist2 = {
+    title: "Hip-hop Hits",
+    coverImageUrl: "./hip_hop_hits.jpg",
+    info: {
+        totalTracksCount: 4,
+        totalTracksDurationInSeconds: 733
+    },
+    tracks: [
+        {
+            coverImageUrl: "./track1.jpg",
+            artist: "Eminem",
+            title: "Rap God",
+            fileUrl: "./Eminem_-_Rap_God_.mp3",
+            isHot: false
+        },
+        {
+            coverImageUrl: "./track2.jpg",
+            artist: "50 cent",
+            title: "In da Club",
+            fileUrl: "./50_Cent_-_In_Da_Club.mp3",
+            isHot: true
+        },
+    ]
+}
+
+// render
+renderPlaylist(playlist)
+renderPlaylist(playlist2)
+
+function renderPlaylist(playlistForRendering) {
+    renderPlaylistHeader(playlistForRendering)
+    renderTrack(playlistForRendering)
+}
+
+function renderPlaylistHeader(inputPlaylistForRendering){
+    // здесь логика отрисовки шапки "входного" плейлиста
+    let playListTitleElement = document.createElement('h2')
+    playListTitleElement.append(inputPlaylistForRendering.title)
+    document.body.append(playListTitleElement)
+
+    let playListCoverElement = document.createElement('img')
+    playListCoverElement.src = inputPlaylistForRendering.coverImageUrl
+    document.body.append(playListCoverElement)
+}
+
+function renderTrack(inputTrackForRendering){
+    // здесь логика отрисовки "входного" трека
+    let tracksListElement = document.createElement('ul')
+
+    for (let i = 0; i < inputTrackForRendering.tracks.length; i++) {
+        let trackElement = document.createElement('li')
+        trackElement.title = inputTrackForRendering.tracks[i].title
+        trackElement.artist = inputTrackForRendering.tracks[i].artist
+        trackElement.append(trackElement.artist + " - " + trackElement.title)
+        tracksListElement.append(trackElement)
+        document.body.append(tracksListElement)
+    }
+}
 
 
-let playListCoverElement = document.createElement('img')
-playListCoverElement.src = playlist.coverImageUrl
-document.body.append(playListCoverElement)
-
-let tracksListElement = document.createElement('ul')
-
-let track1Element = document.createElement('li')
-track1Element.title = playlist.tracks[0].title
-track1Element.artist = playlist.tracks[0].artist
-track1Element.append(track1Element.artist + " - " + track1Element.title)
 
 
-let track2Element = document.createElement('li')
-track2Element.title = playlist.tracks[1].title
-track2Element.artist = playlist.tracks[1].artist
-track2Element.append(track2Element.artist + " - " + track2Element.title)
-
-tracksListElement.append(track1Element, track2Element)
 
 
-document.body.append(tracksListElement)
 
-let track1mp3 = document.createElement("audio")
-track1mp3.audio = playlist.tracks[0].fileUrl
-track1mp3.append(track1mp3.audio)
-document.body.append(track1mp3)
+
+
+
+
