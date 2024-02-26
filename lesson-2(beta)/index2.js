83b93a37964dfb6e7d4ec1ec38da4bdb0a532b93
zaytsev <zaicev@mniti.ru>
2024-02-26 16:31:42 +0300

    lesson 2

--- lesson-2(beta)/index2.js
+++ lesson-2(beta)/index2.js
@@ -2,6 +2,7 @@
 const playlist = {
     title: "Hip-Hop Hits",
     coverImageUrl: "./hip_hop_hits.jpg",
+    coverHotUrl: "./isHot.jpg",
     info: {
         totalTracksCount: 4,
         totalTracksDurationInSeconds: 733,
@@ -28,20 +29,51 @@ const playlist = {
 renderPlaylist(playlist)
 
 function renderPlaylist(playlistForRendering) {
+    renderPlaylistHeader(playlistForRendering)
+    renderPlaylistTracks(playlistForRendering)
+}
+
+function renderPlaylistHeader(playlistForRendering) {
+    const albumImageElement = document.createElement("div")
     let playlistTitle = document.createElement('h2')
     playlistTitle.append(playlistForRendering.title)
     document.body.append(playlistTitle)
 
     let playlistCoverImage = document.createElement('img')
     playlistCoverImage.src = playlistForRendering.coverImageUrl
-    document.body.append(playlistCoverImage)
+    albumImageElement.append(playlistCoverImage)
+    document.body.append(albumImageElement)
+}
 
+function renderPlaylistTracks(playlistForRendering) {
+    
+    
     for (let i = 0; i < playlistForRendering.tracks.length; i++) {
+        
+        const trackElement = document.createElement("div")
+        
         let coverTrackImageUrl = document.createElement('img')
         coverTrackImageUrl.src = playlistForRendering.tracks[i].coverImageUrl
-        
         document.body.append(coverTrackImageUrl)
+
+       
+        
+        let trackTitleElement = document.createElement("div")
+        
+        if(playlistForRendering.tracks[i].isHot) {
+            let coverHotImageUrl = document.createElement('img')
+            coverHotImageUrl.src = playlistForRendering.coverHotUrl
+            trackTitleElement.append(coverHotImageUrl)
+        }
+        trackTitleElement.append(playlistForRendering.tracks[i].artistName + " - " + playlistForRendering.tracks[i].title)
+
+        let playerElement = document.createElement('audio')
+        playerElement.src = playlistForRendering.tracks[i].fileUrl
+        playerElement.controls = true
+
+        trackElement.append(trackTitleElement, playerElement)
+        document.body.append(trackElement)
     }
 
-
+    
 }
