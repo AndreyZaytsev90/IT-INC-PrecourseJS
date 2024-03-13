c08df82c1da3505a57b8562810bd597c1dc2bcbc
zaytsev <zaicev@mniti.ru>
2024-03-13 10:25:47 +0300

    lesson 2 complete

--- lesson-2(beta)/player/src/components/playlist/renderPlaylist.component.js
+++ lesson-2(beta)/player/src/components/playlist/renderPlaylist.component.js
@@ -1,8 +1,10 @@
 import {renderPlaylistHeader} from './header/renderHeader.component.js'
 import {renderPlaylistTracks} from './tracks/renderTracks.component.js'
+import {renderSeparator} from "../../common/renderSeparator.component.js";
 
 export function renderPlaylist(playlistForRendering) {
     renderPlaylistHeader(playlistForRendering)
     renderPlaylistTracks(playlistForRendering)
+    renderSeparator()
 }
 
