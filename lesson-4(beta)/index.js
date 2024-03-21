e7716b748961024ce46ef01a97ecc0ebee219ded
zaytsev <zaicev@mniti.ru>
2024-03-21 16:55:05 +0300

    lesson 4 in progress

--- lesson-4(beta)/index.js
+++ lesson-4(beta)/index.js
@@ -1,2 +1,8 @@
 import {data} from './data/data.js'
-import {renderTodolist} from './data/data.js'
+import {Todolist} from './components/todolist.js'
+
+const todolist = new Todolist(data.todolist)
+
+const root = document.getElementById('root')
+
+root.append(todolist)
