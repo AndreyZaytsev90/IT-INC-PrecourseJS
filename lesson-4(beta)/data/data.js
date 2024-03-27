bc6e9534727941f2e024e2dd7edc921fa7193924
zaytsev <zaicev@mniti.ru>
2024-03-27 16:54:32 +0300

    Update file

--- lesson-4(beta)/data/data.js
+++ lesson-4(beta)/data/data.js
@@ -1,21 +1,43 @@
 export const data = {
     todolist: {
-        id: createUniqId(),
+        id: generateId(),
         title: "Want to Learn",
         tasks: [
             {
-                id: createUniqId(),
+                id: generateId(),
                 title: "Learn HTML",
             },
             {
-                id: createUniqId(),
+                id: generateId(),
                 title: "Learn CSS",
             },
         ]
     }
 }
 
+function generateId() {
+    const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const charsLength = chars.length;
+    const sections = [8, 4, 4, 4, 12]; // Длины каждой секции ID
 
-function createUniqId(){
-    return Math.floor(Math.random()*10000000)
+    let id = '';
+
+    for (let i = 0; i < sections.length; i++) {
+        for (let j = 0; j < sections[i]; j++) {
+            id += chars.charAt(Math.floor(Math.random() * charsLength));
+        }
+        if (i < sections.length - 1) {
+            id += '-';
+        }
+    }
+
+    return id;
+}
+
+function addTask(data) {
+   const newTask = {
+       id: generateId(),
+       title: "--------",
+   }
+   data.todolist.tasks.push(newTask)
 }
