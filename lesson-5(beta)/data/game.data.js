3095346109dbc7bd487e3bb3c03ea997bc7c08fc
zaytsev <zaicev@mniti.ru>
2024-04-03 15:24:47 +0300

    Update file

---
+++ lesson-5(beta)/data/game.data.js
@@ -0,0 +1,30 @@
+export const data = {
+    settings: {
+        rowsCount: 3,
+        columnCount: 3,
+        pointsToWin: 10,
+        maximumMisses: 3,
+        decreaseDeltaInMs: 100,
+        inMuted: true
+    },
+    coords: {
+        current: {
+            x: 0,
+            y: 0
+        },
+        caught: {
+            x: 0,
+            y: 0
+        },
+        missed: {
+            x: 0,
+            y: 0
+        },
+    },
+    score: {
+        missCount: 2,
+        catchCount: 1
+    }
+
+
+}
---
+++ lesson-5(beta)/ui/game/game.component.js
@@ -0,0 +1,5 @@
+export function Game() {
+    const containerElement = document.createElement('div')
+   containerElement.append('demo')
+    return containerElement
+}
