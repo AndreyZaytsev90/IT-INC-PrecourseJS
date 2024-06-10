0bd2b1e7ba55373a289b800cd576ac8735c7b88d
zaytsev <zaicev@mniti.ru>
2024-06-10 16:47:56 +0300

    update file

--- miniCourse/core/state-manager.js
+++ miniCourse/core/state-manager.js
@@ -8,5 +8,11 @@ const _state = {
 export const getGooglePoints = () => _state.points.google
 
 export const getPlayersPoints = (playerNumber) => {
-    
+    if (playerNumber > 0){
+        return _state.points.players[playerNumber - 1];
+    } else {
+        console.log("Номер игрока не может быть меньше 1")
+        return null
+    }
+   
 }
--- miniCourse/ui/miniCourse.js
+++ miniCourse/ui/miniCourse.js
@@ -1,5 +1,5 @@
 /*import {_state as state} from "../core/state-manager.js"*/
-import {getGooglePoints} from "../core/state-manager.js"
+import {getGooglePoints, getPlayersPoints} from "../core/state-manager.js"
 
 const rootElement = document.getElementById('root')
 rootElement.innerHTML = ''
@@ -10,5 +10,10 @@ rootElement.innerHTML = ''
     rootElement.append(` Player${numberPlayer}: ${score.toString()}`)
 }*/
 
+const player1 = getPlayersPoints(1)
+const player2 = getPlayersPoints(2)
+
 const google = getGooglePoints()
 rootElement.append(` Google: ${google}`)
+rootElement.append(` Player1: ${player1}`)
+rootElement.append(` Player2: ${player2}`)
